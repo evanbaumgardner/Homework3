c6d9b0478ac5278897f67dad1d4003aa0614ea46
Evan Baumgardner <baumer@Goliath.local>
2014-05-15 13:27:39 -0700

    Initial Commit

---
+++ class3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  class3
+//
+//  Created by Evan Baumgardner on 5/15/14.
+//  Copyright (c) 2014 Evan Baumgardner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
